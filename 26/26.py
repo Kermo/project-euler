62ca6fe9e92e812f1a44b6dee88418fd60fc4d8a
Jani Keränen <jani_keranen@msn.com>
2019-12-07 21:47:05 +0200

    Update 26.py

--- 26/26.py
+++ 26/26.py
@@ -1,22 +1,18 @@
 def find_longest_cycle():
-	cycle = []
-	for x in range(2, 1000):
-		threshold = 1000
-		left = 10
-		while(threshold > 0):
-			left = left % x
-			cycle.append(left)
-			
-			print(cycle)
-			
-			if left == 0:
-				break
-			
-			left = left * 10
-			
-			threshold =- 1
+	num = longest = 1
+	for x in range(3, 1000, 2):
 		
-	return len(cycle)
+		if x % 5 == 0:
+			continue
+			
+		cycle = 1
+		
+		while (10 ** cycle) % x != 1:
+			cycle += 1
+		
+		if cycle > longest:
+			num, longest = x, cycle
+		
+	return num
 			
 print(str(find_longest_cycle()))
-
